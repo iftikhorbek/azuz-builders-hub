f7dbc61fc72e7752b453af71753fea0c33e05499
Iftihor <iftihor@xt.uz>
2025-10-16 18:33:15 +0500

    deploying

---
+++ config/vite.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Build Path
+    |--------------------------------------------------------------------------
+    |
+    | This value determines where the Vite build assets are output. It should
+    | correspond to the directory configured in your Vite build settings.
+    |
+    */
+
+    'build_path' => 'dist',
+];
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    build: {
+        outDir: 'public/dist',
+        emptyOutDir: true,
+        manifest: true,
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
